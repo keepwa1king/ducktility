ed88dbbc0360e4599d3a0ed0428875bb9b874662
shinyo17 <shunrises@gmail.com>
2024-07-27 18:29:47 +0900

    ui: fix floating button margin

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -16,7 +16,7 @@ export default function Button(props: ButtonProps) {
   };
 
   return (
-    <button class={`fixed bottom-0 left-0 right-0 mx-auto my-12 max-w-app`} onClick={props.onClick}>
+    <button class={`fixed bottom-0 left-0 right-0 mx-auto mb-6 max-w-app`} onClick={props.onClick}>
       <span
         class={`${variantClasses[props.variant]} mx-5 flex h-12 flex-col items-center justify-center rounded-lg px-9 py-4 text-base font-semibold leading-[18px] tracking-[-0.1px] text-white`}
       >
